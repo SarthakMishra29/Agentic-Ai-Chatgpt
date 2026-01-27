df5c8c34fb7bf2d04ab348d9e8ab2dbd7f5ce1d8
Sarthak_Mishra <mishrasarthak29p@gmail.com>
2026-01-27 19:35:30 +0530

    Agentic-Ai/Chatgpt completed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
-
-/* Routes */
+// /* Routes */
 const authRoutes = require("./routes/auth.routes");
 const chatRoutes = require("./routes/chat.routes");
 
@@ -12,14 +11,16 @@ const app = express();
 /* using middlewares */
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../public")));
 
-/* Using Routes */
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+// /* Using Routes */
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
-
-app.get("*name", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
 
 module.exports = app;
