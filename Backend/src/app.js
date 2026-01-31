416eead5ce51ac257a4332135a9e4ee58a416122
Sarthak_Mishra <mishrasarthak29p@gmail.com>
2026-01-31 15:25:30 +0530

    Add initial HTML and SVG assets for frontend setup
    - Created index.html with basic structure and linked assets
    - Added vite.svg for favicon

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,25 +2,30 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
-// /* Routes */
+
+/* Routes */
 const authRoutes = require("./routes/auth.routes");
 const chatRoutes = require("./routes/chat.routes");
 
 const app = express();
 
 /* using middlewares */
-app.use(express.json());
-app.use(cookieParser());
-
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   }),
 );
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "../public")));
 
-// /* Using Routes */
+/* Using Routes */
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
+
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 module.exports = app;
